3cb256c92fbcae62d906fa21d6aa491998c0d3c5
Juanansal <Juanansal@gmail.com>
2019-01-02 01:07:30 +0100

    eses

--- app.js
+++ app.js
@@ -3,37 +3,37 @@ var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
 var app = express();
 
-    // Config
-    conf = require('./config.js');
-    console.log(conf.rush.db.user);
+// Config
+conf = require('./config.js');
+console.log(conf.rush.db.user);
 
 
-    app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT || 5000));
 
 
-	app.listen(app.get('port'), function() {
-	  console.log("Servidor configurado por puerto: "+app.get('port'));
-	});
+app.listen(app.get('port'), function() {
+    console.log("Servidor configurado por puerto: "+app.get('port'));
+});
 
-    app.use(express.static(__dirname + '/web'));	
-	
-	console.log('------------------------------------------------');
-	console.log('------------------------------------------------');
+app.use(express.static(__dirname + '/web'));	
 
-    mongoose.connect('mongodb://dumde:manco@ds055535.mlab.com:55535/exodarrushhelper', function(error){
-       if(error)
-	   {
-	      throw error; 	  
-       }else
-       
-	   {		   
-           
-          console.log('Conectado a la base de datos');
-       }
-    });
+console.log('------------------------------------------------');
+console.log('------------------------------------------------');
 
-    // Modelos
-    require('./modelos/peticiones/Rush.js')(app);
+//mongoose.connect('mongodb://dumde:manco@ds055535.mlab.com:55535/exodarrushhelper', function(error){
+mongoose.connect('mongodb://dumde:manco@ds055535.mlab.com:55535/exodarrushhelper', function(error){
+if(error)
+{
+    throw error; 	  
+}else
+
+{		   
+    
+    console.log('Conectado a la base de datos');
+}
+});
+
+// Modelos
+require('./modelos/peticiones/Rush.js')(app);
 
 
-	
