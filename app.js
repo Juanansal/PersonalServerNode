8bd723012c2318e4eb5cd0db0d8c4273f96c27a7
Juanansal <Juanansal@gmail.com>
2019-01-02 07:56:50 +0100

    ddddddd

--- app.js
+++ app.js
@@ -5,15 +5,9 @@ var fs = require('fs');
 var app = express();
 
 // Config
-var confRush = {};
-if (fs.existsSync("./config/rush.js")) {
-    var confRush = require('./config/rush.js');
-}
-else {
-    confRush.user = process.env.RUSH_USUARIO_DB;
-    confRush.pass = process.env.RUSH_PASS_DB;
-}
-
+var conf = require('./config/config.js')();
+console.log(conf);
+console.log(conf.rush.user);
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -22,14 +16,12 @@ app.listen(app.get('port'), function() {
     console.log("Servidor configurado por puerto: "+app.get('port'));
 });
 
-console.log(process.env.RUSH_PASS_DB);
-
 app.use(express.static(__dirname + '/web'));	
 
 console.log('------------------------------------------------');
 console.log('------------------------------------------------');
 
-mongoose.connect('mongodb://'+confRush.user+':'+confRush.pass+'@ds055535.mlab.com:55535/exodarrushhelper', function(error){
+mongoose.connect('mongodb://'+conf.rush.user+':'+conf.rush.pass+'@ds055535.mlab.com:55535/exodarrushhelper', function(error){
 //mongoose.connect('mongodb://'+process.env.RUSH_USUARIO_DB+':'+process.env.RUSH_PASS_DB+'@ds055535.mlab.com:55535/exodarrushhelper', function(error){
 if(error)
 {
---
+++ config/config.js
@@ -0,0 +1,23 @@
+
+var fs = require('fs');
+
+module.exports = init;
+
+function init() {
+
+    var enviar;
+
+    if (fs.existsSync("./config/local.js")) {
+        enviar = require('../config/local.js');   
+    }
+    else {
+        enviar = {
+            rush: {
+                user: process.env.RUSH_USUARIO_DB,
+                pass: process.env.RUSH_PASS_DB
+            }
+        }
+    }
+    return enviar;
+}
+    
