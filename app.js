1d626d2de32b2e6cb35d24239b7e6832aa068254
Juanansal <Juanansal@gmail.com>
2019-01-05 02:33:51 +0100

    ooo

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var MongoClient = require('mongodb').MongoClient;
 var mongoose = require('mongoose');
+var cors = require('cors');
 var fs = require('fs');
 var app = express();
 
@@ -16,6 +17,7 @@ require('./api/peticiones/meteo.js')(app,conf);
 
 // Config del servidor
 app.set('port', (process.env.PORT || 5000));
+app.use(cors());
 app.listen(app.get('port'), function() {
     console.log('------------------------------------------------');
     console.log('------------------------------------------------');
