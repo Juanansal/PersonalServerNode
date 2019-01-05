a7718c310dd9d4f7ce279c0843a84432dc3ebb04
Juanansal <Juanansal@gmail.com>
2019-01-05 02:17:41 +0100

    G

--- api/peticiones/meteo.js
+++ api/peticiones/meteo.js
@@ -25,7 +25,7 @@ module.exports = function(app,conf)
 
 
 
-    app.get('/api/meteo/test', function(req, res){
+    app.get('/api/meteo/test2', function(req, res){
 
         res.send('LUL');
     });
