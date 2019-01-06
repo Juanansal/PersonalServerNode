58a730c56fe4e80c88f2b4076bb8facc7a9c94ad
Juanansal <Juanansal@gmail.com>
2019-01-06 02:33:35 +0100

    yyy

--- public/meteo/js/init.js
+++ public/meteo/js/init.js
@@ -2,8 +2,5 @@
 var meteo = {
    estaciones: {},
    datosAmostrar: {},
-   graficaAmostrar: 0,
-   apiKey: 'eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJqdWFuYW5zYWxAZ21haWwuY29tIiwianRpIjoiZmEzZDA4ZjEtNzk5ZC00YWZhLWE1ZTMtMmRjZjVkYjFhYzQ1IiwiaXNzIjoiQUVNRVQiLCJpYXQiOjE1MzMzMzUwNTYsInVzZXJJZCI6ImZhM2QwOGYxLTc5OWQtNGFmYS1hNWUzLTJkY2Y1ZGIxYWM0NSIsInJvbGUiOiIifQ.CRv5ZCxZPYAFpLGPY5t8p7kPGi_AxhGBfoIV_TVOdls'
+   graficaAmostrar: 0
 };
-
-// 2018-11-14T20:00:00UTC
